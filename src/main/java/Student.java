d571b1238bfa5c789608614e4d941d0d3ea18e4c
Alan Shaughnessy <alan.shaughnessy123@gmail.com>
2019-12-02 15:43:52 +0000

    added three classes with student completed

---
+++ src/main/java/Student.java
@@ -0,0 +1,65 @@
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+
+	private String name = "";
+	private int age;
+	private int iD;
+	private Date dateOfBirth;
+	private List<Course> courses = new ArrayList<Course>();
+	private List<Module> modules = new ArrayList<Module>();
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String newName) {
+		this.name = newName;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int newAge) {
+		this.age = newAge;
+	}
+	
+	public int getId() {
+		return iD;
+	}
+	
+	public void setId(int newId) {
+		this.iD = newId;
+	}
+	
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	
+	public void setDateOfBirth(Date newDOB) {
+		this.dateOfBirth = newDOB;
+	}
+	
+	public String getUsername() {
+		return new String(name+iD);
+	}
+	
+	public void addCourse(Course newCourse) {
+		courses.add(newCourse);
+	}
+	
+	public List<Course> getCourses(){
+		return courses;
+	}
+	
+	public void addModule(Module newModule) {
+		modules.add(newModule);
+	}
+	
+	public List<Module> getModules(){
+		return modules;
+	}
+}
