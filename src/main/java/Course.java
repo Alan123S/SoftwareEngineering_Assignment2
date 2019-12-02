7034f9288086afd7c473c43d43dfb2a6a1cf80fb
Alan Shaughnessy <alan.shaughnessy123@gmail.com>
2019-12-02 17:41:26 +0000

    added DateTime dependency and completed Course and Module classes

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,4 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+import org.joda.time.DateTime;
 
 public class Course {
 
+	private String name = "";
+	private List<Student> students = new ArrayList<Student>();
+	private List<Module> modules = new ArrayList<Module>();
+	private DateTime startDate = new DateTime();
+	private DateTime endDate = new DateTime();
+	
+	public void setName(String newName) {
+		this.name = newName;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void addStudent(Student newStudent) {
+		students.add(newStudent);
+	}
+	public List<Student> getStudents() {
+		return students;
+	}
+	
+	public void addModule(Module newModule) {
+		modules.add(newModule);
+	}
+	public List<Module> getModules() {
+		return modules;
+	}
+	
+	public void setStartDate(DateTime newStartDate) {
+		this.startDate = newStartDate;
+	}
+	public DateTime getStartDate() {
+		return startDate;
+	}
+	
+	public void setEndDate(DateTime newEndDate) {
+		this.endDate = newEndDate;
+	}
+	public DateTime getEndDate() {
+		return endDate;
+	}
 }
--- src/main/java/Module.java
+++ src/main/java/Module.java
@@ -1,4 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Module {
 
+	private String name = "";
+	private int id;
+	private List<Student> students = new ArrayList<Student>();
+	private List<Course> courses = new ArrayList<Course>();
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String newName) {
+		this.name = newName;
+	}
+	
+	public void setId(int newId) {
+		this.id = newId;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void addStudent(Student newStudent) {
+		students.add(newStudent);
+	}
+	
+	public List<Student> getStudents() {
+		return students;
+	}
+	
+	public void addCourse(Course newCourse) {
+		courses.add(newCourse);
+	}
+	
+	public List<Course> getCourses() {
+		return courses;
+	}
 }
