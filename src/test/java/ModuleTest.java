3864bced13979633ec76413e0960f057040a6fb3
Alan Shaughnessy <alan.shaughnessy123@gmail.com>
2019-12-22 23:55:52 +0000

    add tests for Module

--- src/test/java/ModuleTest.java
+++ src/test/java/ModuleTest.java
@@ -1,52 +1,80 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.joda.time.DateTime;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ModuleTest {
 
+	Module module;
+	Student newStudent = new Student("studName", 10, 1111, new Date(), new ArrayList<Course>(), new ArrayList<Module>());
+	Course newCourse = new Course("courseName", new ArrayList<Student>(), new ArrayList<Module>(), new DateTime(), new DateTime());
+	
+	@Before
+	public void setup() throws Exception {
+		module = new Module("defaultName", 414, new ArrayList<Student>(), new ArrayList<Course>());
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		module = null;
+		assertEquals(null, module);
+	}
+	
 	@Test
 	public void testModule() {
-		fail("Not yet implemented");
+		assertNotEquals(null, module);
 	}
 
 	@Test
 	public void testGetName() {
-		fail("Not yet implemented");
+		assertEquals("defaultName", module.getName());
 	}
 
 	@Test
 	public void testSetName() {
-		fail("Not yet implemented");
+		module.setName("newName");
+		assertEquals("newName", module.getName());
 	}
 
 	@Test
 	public void testSetId() {
-		fail("Not yet implemented");
+		module.setId(11);
+		assertEquals(11, module.getId());
 	}
 
 	@Test
 	public void testGetId() {
-		fail("Not yet implemented");
+		module.setId(11);
+		assertEquals(11, module.getId());
 	}
 
 	@Test
 	public void testAddStudent() {
-		fail("Not yet implemented");
+		module.addStudent(newStudent);
+		assertEquals(newStudent, module.getStudents().get(0));
 	}
 
 	@Test
 	public void testGetStudents() {
-		fail("Not yet implemented");
+		module.addStudent(newStudent);
+		assertEquals(newStudent, module.getStudents().get(0));
 	}
 
 	@Test
 	public void testAddCourse() {
-		fail("Not yet implemented");
+		module.addCourse(newCourse);
+		assertEquals(newCourse, module.getCourses().get(0));
 	}
 
 	@Test
 	public void testGetCourses() {
-		fail("Not yet implemented");
+		module.addCourse(newCourse);
+		assertEquals(newCourse, module.getCourses().get(0));
 	}
 
 }
